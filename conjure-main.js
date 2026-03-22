8c8b16ca49961cffdda61b97adbab75ba58c9f52
itsjustTimmy <oyetunjitimilehin03@gmail.com>
2026-03-22 10:42:09 +0100

    renamed conjure.html - index.html

--- conjure-main.js
+++ conjure-main.js
@@ -7,3 +7,35 @@ function toggleMenu() {
 }
 
 menuBtn.addEventListener('click', toggleMenu);
+
+const cartBtn = document.getElementById('cart-btn')
+const cartCount = document.getElementById('cart-count')
+const shopNowBtn = document.querySelector('.btn-primary')
+
+let count = 0;
+function addToCart() {
+    count++;
+    cartCount.textContent = count;
+    cartCount.classList.add('visible')
+    cartCount.classList.remove('bump');
+    setTimeout(() => {
+        cartCount.classList.add('bump');
+    }, 10);
+}
+
+shopNowBtn.addEventListener('click', function (event) {
+    event.preventDefault(); 
+    addToCart();
+});
+
+const oneLink = document.querySelector('.nav-links a');
+const allLinks = document.querySelectorAll('.nav-links a');
+
+allLinks.forEach(function(link){
+    console.log(link);
+})
+
+const target = link.getAttribute('href');
+const section = document.querySelector(target);
+
+section.scrollIntoView({ behavior: 'smooth' });
